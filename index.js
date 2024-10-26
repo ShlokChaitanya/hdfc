119e9804b16ecb5bd95ab3c1ef47961e93d297a9
shlokchaitanya <shlokchaitanya120@gmail.com>
2024-10-26 09:44:19 +0530

    v2 commit

--- index.js
+++ index.js
@@ -3,25 +3,25 @@ const path = require('path');
 let initial_path = process.cwd();
 const hdfc = express();
 
-
-hdfc.use(express.static(initial_path));
+hdfc.use(express.static((path.join(__dirname, 'public'))));
 
 hdfc.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'netbanking', 'index.html'));
+    res.sendFile(path.join(initial_path, 'public', 'netbanking', 'index.html'));
 });
 
-hdfc.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'login', 'index.html'));
+hdfc.get('/login', (req, res) =>     {
+    res.sendFile(path.join(initial_path, 'public', 'login', 'index.html'));
 });
 
 hdfc.get('/entry', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Dashboard', 'index.html'));
+    res.sendFile(path.join(initial_path, 'public', 'Dashboard', 'index.html'));
 });
 
 hdfc.get('/statement', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Statement', 'index.html'));
+    res.sendFile(path.join(initial_path, 'public', 'Statement', 'index.html'));
 });
 
-hdfc.listen("3002", () => {
-    console.log('listening......');
-})
+const PORT = process.env.PORT || 3002;
+hdfc.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+});
