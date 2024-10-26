c7bf69493e7863bf4cf633c61e725e917ec23d70
shlokchaitanya <shlokchaitanya120@gmail.com>
2024-10-26 10:04:28 +0530

    v2 inital

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const path = require('path');
 let initial_path = process.cwd();
 const hdfc = express();
 
-hdfc.use(express.static((path.join(__dirname, 'public'))));
+hdfc.use(express.static((path.join(__dirname))));
 
 hdfc.get('/', (req, res) => {
-    res.sendFile(path.join(initial_path, 'public', 'netbanking', 'index.html'));
+    res.sendFile(path.join(initial_path, 'netbanking', 'index.html'));
 });
 
 hdfc.get('/login', (req, res) =>     {
-    res.sendFile(path.join(initial_path, 'public', 'login', 'index.html'));
+    res.sendFile(path.join(initial_path, 'netbanking', 'login', 'index.html'));
 });
 
 hdfc.get('/entry', (req, res) => {
-    res.sendFile(path.join(initial_path, 'public', 'Dashboard', 'index.html'));
+    res.sendFile(path.join(initial_path, 'netbanking', 'Dashboard', 'index.html'));
 });
 
 hdfc.get('/statement', (req, res) => {
-    res.sendFile(path.join(initial_path, 'public', 'Statement', 'index.html'));
+    res.sendFile(path.join(initial_path, 'netbanking', 'Statement', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3002;
